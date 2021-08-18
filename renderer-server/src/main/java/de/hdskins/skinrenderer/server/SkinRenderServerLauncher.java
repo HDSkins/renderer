7ac13b46a30163a492d8f641b2f5637e0ecc2feb
derrop <derrop@outlook.de>
2021-08-18 20:02:45 +0200

    Add support for configuration through environment variables

--- renderer-server/src/main/java/de/hdskins/skinrenderer/server/SkinRenderServerLauncher.java
+++ renderer-server/src/main/java/de/hdskins/skinrenderer/server/SkinRenderServerLauncher.java
@@ -4,17 +4,26 @@ import com.github.derrop.documents.Document;
 import com.github.derrop.documents.Documents;
 import de.hdskins.skinrenderer.shared.RabbitMQConnector;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.TimeoutException;
 
 public class SkinRenderServerLauncher {
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) {
+        String rendererName = System.getenv("SKIN_RENDERER_NAME");
+        String queue = System.getenv("SKIN_RENDERER_QUEUE");
+        String rawPriority = System.getenv("SKIN_RENDERER_PRIORITY");
+        String rawRendererCount = System.getenv("SKIN_RENDERER_THREAD_COUNT");
+        String rabbitMQHost = System.getenv("SKIN_RENDERER_RABBITMQ_HOST");
+        String rabbitMQPort = System.getenv("SKIN_RENDERER_RABBITMQ_PORT");
+        String rabbitMQUser = System.getenv("SKIN_RENDERER_RABBITMQ_USER");
+        String rabbitMQPass = System.getenv("SKIN_RENDERER_RABBITMQ_PASSWORD");
+
+        boolean useConfig = rendererName == null || queue == null || rabbitMQHost == null || rabbitMQUser == null || rabbitMQPass == null;
+
         Path path = Paths.get("config.yml");
-        if (!Files.exists(path)) {
+        if (!Files.exists(path) && useConfig) {
             Documents.newDocument()
                     .append("rendererName", "Renderer1")
                     .append("queue", "skinrender")
@@ -28,7 +37,36 @@ public class SkinRenderServerLauncher {
 
         Document config = Documents.yamlStorage().read(path);
 
-        SkinRenderServer server = SkinRenderServer.create(config.getString("rendererName"), config.getString("queue"), config.getInt("priority"), config.get("rabbitMQ", RabbitMQConnector.class), config.getInt("rendererCount"));
+        RabbitMQConnector connector;
+        int priority = 1;
+        int rendererCount = 1;
+
+        if (useConfig) {
+            rendererName = config.getString("rendererName");
+            queue = config.getString("queue");
+            priority = config.getInt("priority");
+            connector = config.get("rabbitMQ", RabbitMQConnector.class);
+            rendererCount = config.getInt("rendererCount");
+        } else {
+            try {
+                rendererCount = Integer.parseInt(rawRendererCount);
+            } catch (NumberFormatException ignored) {
+            }
+            try {
+                priority = Integer.parseInt(rawPriority);
+            } catch (NumberFormatException ignored) {
+            }
+
+            int port = 5672;
+            try {
+                port = Integer.parseInt(rabbitMQPort);
+            } catch (NumberFormatException ignored) {
+            }
+
+            connector = new RabbitMQConnector(rabbitMQHost, port, rabbitMQUser, rabbitMQPass);
+        }
+
+        SkinRenderServer server = SkinRenderServer.create(rendererName, queue, priority, connector, rendererCount);
         server.start();
     }
 }
